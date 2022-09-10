2c337c5e6a49b74a35240d366e81bfed2029e61b
Enes Koç <suateneskoc@gmail.com>
2022-09-10 15:20:23 +0300

    feature: github link

--- src/App.js
+++ src/App.js
@@ -79,33 +79,44 @@ function App() {
     return () => {
       window.removeEventListener("keydown", handleKeyPress);
     };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div
-        id="drum-machine"
-        className="flex flex-wrap justify-between gap-y-4 w-80"
-      >
-        {drumPads.map((pad) => (
-          <button
-            className="drum-pad w-[30%] aspect-square rounded-md bg-gray-100 text-xl hover:bg-gray-200 active active:bg-gray-300 transition"
-            id={pad.id}
-            onClick={() => playSound(pad.key.toUpperCase())}
-            key={pad.id}
-          >
-            {pad.key}
-            <audio src={pad.src} className="clip" id={pad.key}></audio>
-          </button>
-        ))}
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <div className="flex items-center flex-1">
         <div
-          id="display"
-          className="w-full h-24 flex items-center justify-center bg-gray-200 rounded-md text-2xl"
+          id="drum-machine"
+          className="flex flex-wrap justify-between gap-y-4 w-80"
         >
-          {displayText}
+          {drumPads.map((pad) => (
+            <button
+              className="drum-pad w-[30%] aspect-square rounded-md bg-gray-100 text-xl hover:bg-gray-200 active active:bg-gray-300 transition"
+              id={pad.id}
+              onClick={() => playSound(pad.key.toUpperCase())}
+              key={pad.id}
+            >
+              {pad.key}
+              <audio src={pad.src} className="clip" id={pad.key}></audio>
+            </button>
+          ))}
+          <div
+            id="display"
+            className="w-full h-24 flex items-center justify-center bg-gray-200 rounded-md text-2xl"
+          >
+            {displayText}
+          </div>
         </div>
       </div>
+      <span className="mb-2">
+        View code on{" "}
+        <a
+          href="https://github.com/suateneskoc/freecodecamp-drum-machine"
+          className="text-blue-600 font-semibold"
+        >
+          GitHub
+        </a>
+      </span>
     </div>
   );
 }
